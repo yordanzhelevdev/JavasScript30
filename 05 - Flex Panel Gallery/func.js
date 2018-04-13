f361fb8d0b99adab549e350815eba50ebebc230c
yordanzhelevdev <yordanzhelev950711@gmail.com>
2018-04-14 01:01:24 +0300

    Completed: 05 - Flex Panel Gallery

--- 05 - Flex Panel Gallery/func.js
+++ 05 - Flex Panel Gallery/func.js
@@ -3,13 +3,14 @@
 	const panels = document.querySelectorAll('div.panel');
 	console.log(panels)
 	Array.from(panels).forEach( (panel) => {
-		panel.addEventListener('click', (e) => {
-			const panel = e.target;
-			panel.classList.toggle('open');
-			panel.classList.toggle('open-active');
-			console.log(panel); 
-			
+		panel.addEventListener('click', () => {
+			this.classList.toggle('open');
 		});
+		panel.addEventListener('transitionend', (e) => {
+			if(e.propertyName.includes('flex')){
+					this.classList.toggle('open-active');
+			}
+		})
 	});
 
 }());
