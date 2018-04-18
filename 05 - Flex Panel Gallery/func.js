83cc7628f7dd71f9be688135ac80289a0cf5132c
Yordan Zhelev <yordanzhelev950711@gmail.com>
2018-04-18 13:38:36 +0300

    Update func.js

--- 05 - Flex Panel Gallery/func.js
+++ 05 - Flex Panel Gallery/func.js
@@ -1,16 +1,18 @@
-(function() {
-	
-	const panels = document.querySelectorAll('div.panel');
-	console.log(panels)
-	Array.from(panels).forEach( (panel) => {
-		panel.addEventListener('click', () => {
-			this.classList.toggle('open');
-		});
-		panel.addEventListener('transitionend', (e) => {
-			if(e.propertyName.includes('flex')){
-					this.classList.toggle('open-active');
-			}
-		})
-	});
+window.onload = () => {
+	(function() {
 
-}());
+		const panels = document.querySelectorAll('div.panel');
+		console.log(panels)
+		Array.from(panels).forEach( (panel) => {
+			panel.addEventListener('click', () => {
+				this.classList.toggle('open');
+			});
+			panel.addEventListener('transitionend', (e) => {
+				if(e.propertyName.includes('flex')){
+						this.classList.toggle('open-active');
+				}
+			})
+		});
+
+	}());
+}
